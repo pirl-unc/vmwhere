87deae2ec88c7eb130c4a15d0f782574c408b639
skpeterson <skpet@unc.edu>
2025-05-28 14:35:43 -0400

    show example of bash run script

---
+++ examples/run_vmwhere_profile_repeats.sh
@@ -0,0 +1,11 @@
+vmwhere  \
+--sample_id 'test' \
+--bam_file ../path/to/sorted/bam \
+--motif GGAA \
+--fasta ../path/to/fasta \
+--bed_file ../path/to/regions/bed_file \
+--output_dir ../path/to/results/parent/dir \
+--cluster_dist 4 \
+--minor_thresh 0.15 \
+--homozygous_thresh 0.8
+
